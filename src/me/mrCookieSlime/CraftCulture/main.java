57f3f84144ef9eae50ba11dc438e5f2c1f0b5b4a
mrCookieSlime <mrCookieSlime@gmail.com>
2014-04-15 12:45:43 +0200

    Added Config and Plugin.yml

--- src/me/mrCookieSlime/CraftCulture/main.java
+++ src/me/mrCookieSlime/CraftCulture/main.java
@@ -1,7 +1,26 @@
 package me.mrCookieSlime.CraftCulture;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		System.out.println("[CraftCulture] " + "CraftCulture v" + getDescription().getVersion() + " enabled!");
+		
+		loadConfig();
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("CraftCulture v" + getDescription().getVersion() + " disabled!");
+	}
+	
+	public  void loadConfig() {
+		FileConfiguration cfg = getConfig();
+		cfg.options().copyDefaults(true);
+		saveConfig();
+	}
 
 }
